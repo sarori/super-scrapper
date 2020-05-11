2067770671988d8c54950668ac64a238aeff41e3
sarori <salo3678@gmail.com>
2020-05-12 01:14:36 +0200

    Flask - dynamic URLs, templates, Forms and Query Arguments

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/report")
+def report():
+    word = request.args.get("word")
+    return render_template("report.html", searchingBy=word)
+
+
+# app.run(host="127.0.0.1:5000")
